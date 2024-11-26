2d1ccecd7a078dd4b7395c6fab6e72951c7ad76f
ekatzik <elyshkinakate@gmail.com>
2024-11-26 20:00:05 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,1 +1,30 @@
+import random
+import string
 
+def generate_password(length, use_digits=True, use_special_chars=True):
+    "Генерация случайного пароля с заданными условиями."
+    
+    #набор символов по умолчанию:
+    chars = string.ascii_letters
+    
+    #добавляем цифры, если нужно:
+    if use_digits:
+        chars += string.digits
+    
+    #добавляем спец символы, если нужно:
+    if use_special_chars:
+        chars += string.punctuation
+    
+    #генерируем пароль:
+    password = ''.join(random.choice(chars) for _ in range(length))
+    
+    return password
+
+if __name__ == "__main__":
+    #пример использования:
+    length = int(input("Введите длину пароля: "))
+    use_digits = input("Использовать цифры? (y/n): ").lower() == 'y'
+    use_special_chars = input("Использовать специальные символы? (y/n): ").lower() == 'y'
+    
+    password = generate_password(length, use_digits, use_special_chars)
+    print("Сгенерированный пароль:", password)
