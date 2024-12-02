0fd16422ff7ee3da26ffcc9a05d0b17538417090
ekatzik <elyshkinakate@gmail.com>
2024-12-02 17:55:17 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,30 +1,40 @@
 import random
 import string
+import hashlib
 
 def generate_password(length, use_digits=True, use_special_chars=True):
-print("Генерация случайного пароля с заданными условиями.")
+    #Генерация случайного пароля с заданными условиями.
     
-    #набор символов по умолчанию:
+    #набор символов по умолчанию
     chars = string.ascii_letters
     
-    #добавляем цифры, если нужно:
+    #добавляем цифры если нужно
     if use_digits:
         chars += string.digits
     
-    #добавляем спец символы, если нужно:
+    #добавляем специальные символы если нужно
     if use_special_chars:
         chars += string.punctuation
     
-    #генерируем пароль:
+    #генерируем пароль
     password = ''.join(random.choice(chars) for _ in range(length))
     
     return password
 
+def hash_password(password):
+    #Хеширование пароля с использованием алгоритма SHA-256.
+    return hashlib.sha256(password.encode()).hexdigest()
+
 if __name__ == "__main__":
-    #пример использования:
+    #пример использования
     length = int(input("Введите длину пароля: "))
     use_digits = input("Использовать цифры? (y/n): ").lower() == 'y'
     use_special_chars = input("Использовать специальные символы? (y/n): ").lower() == 'y'
     
     password = generate_password(length, use_digits, use_special_chars)
     print("Сгенерированный пароль:", password)
+
+    hash_option = input("Хотите хешировать пароль? (y/n): ").lower()
+    if hash_option == 'y':
+        hashed_password = hash_password(password)
+        print("Хешированный пароль:", hashed_password)
